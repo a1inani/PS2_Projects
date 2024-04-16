a79ab7090982431785569421dd54481578b7388f
Alinani Simukanga <a1inani@users.noreply.github.com>
2024-04-16 22:57:30 +0200

    Update main.c

--- Project_3_TMTC_Tutorial_2/main.c
+++ Project_3_TMTC_Tutorial_2/main.c
@@ -1,6 +1,7 @@
 #include <kernel.h>
 #include <draw.h>
 #include <gs_psm.h>
+#include <gs_gp.h>
 #include <dma.h>
 #include <dma_tags.h>
 #include <graph.h>
@@ -27,6 +28,7 @@ int gs_finish()
 {
     qword_t buf[50];
     qword_t *q = buf;
+    // TODO: cleanup
     q = draw_primitive_xyoffset(q, 0, 0, 0);
     q = draw_finish(q);
     dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
@@ -37,24 +39,44 @@ int gs_finish()
 
 int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
 {
-    unsigned int fb_address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
-    //unsigned int zbuf_address = graph_vram_allocate(w, h, psmz, GRAPH_ALIGN_PAGE);
+    framebuffer_t fb;
+    fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
+    fb.width = width;
+    fb.height = height;
+    fb.psm = psm;
+    fb.mask = 0;
+    
+    zbuffer_t z;
+    z.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
+    z.enable = 0;
+    z.method = 0;
+    z.zsm = psmz;
+    z.mask = 0;
+    
     graph_set_mode(gmode, vmode, GRAPH_MODE_FIELD, GRAPH_DISABLE);
     graph_set_screen(OFFSET_X, OFFSET_Y, width, height);
     graph_set_bgcolor(0, 0, 0);
-    graph_set_framebuffer_filtered(fb_address, width, psm, 0, 0);
+    graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
     graph_enable_output();
 
+    qword_t buf[100];
+    qword_t *q = buf;
+    q = draw_setup_environment(q, 0, &fb, &z);
+    dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
+    dma_wait_fast();
+    
     gs_finish();
 
     return 0;
 }
 
 static int tri[] = {
-    10, 0 , 0,
-    600, 200, 0,
-    20, 400, 0
+    10, 60 , 0,
+    600, 200, 1,
+    20, 400, 10
 };
+
+#define SHIFT_AS_I64(x, b) (((int64_t)x)<<b)
 
 int draw() 
 {
@@ -65,9 +87,13 @@ int draw()
     qword_t buf[50];
     qword_t *q = buf;
     // 6 regs, x1, EOP
-    q->dw[1] = 0x6000000000008001;
+    q->dw[0] = 0x7000000000008001;
     // GIFTag Header - col, pos, col, pos, col, pos
-    q->dw[0] = 0x0000000000515151;
+    q->dw[1] = 0x0000000005151510;
+    q++;
+    
+    q->dw[0] = GS_PRIM_TRIANGLE;
+    q->dw[1] = 0;
     q++;
     
     for(int i = 0; i < 3; i++) {
@@ -75,8 +101,12 @@ int draw()
         q->dw[1] = (blue&0xff) | (0x80 << 32);
         q++;
         
-        q->dw[0] = (tri[i+0]<<4) | (tri[i+1]<<4)<<32;
-        q->dw[0] = (tri[i+2]<<4);
+        // 0xa -> 0xa0
+        // fixed point format xxxx xxxx xxxx.yyyy
+        int base = i*3;
+        q->dw[0] = (tri[base+0]<<4) | SHIFT_AS_I64(tri[base+1]<<4, 32);
+        q->dw[0] = (tri[base+2]<<4);
+        printf("drawing vertex %x %x %x\n", tri[base+0], tri[base+1], tri[base+2]);
         q++;
     }
     
